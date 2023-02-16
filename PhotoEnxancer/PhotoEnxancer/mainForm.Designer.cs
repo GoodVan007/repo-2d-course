9b06cc2ec1226ac4a1ca17bb9219177522a5a7f0
Будаев Иван <budaevivan2003@gmail.com>
2023-02-16 17:34:04 +0500

    brightness

--- PhotoEnxancer/PhotoEnxancer/mainForm.Designer.cs
+++ PhotoEnxancer/PhotoEnxancer/mainForm.Designer.cs
@@ -28,20 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.OriginalPictureBox = new System.Windows.Forms.PictureBox();
+            this.resultPictureBox = new System.Windows.Forms.PictureBox();
+            this.FiltersComboBox = new System.Windows.Forms.ComboBox();
+            this.ApplyButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.OriginalPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resultPictureBox)).BeginInit();
             this.SuspendLayout();
+            // 
+            // OriginalPictureBox
+            // 
+            this.OriginalPictureBox.Location = new System.Drawing.Point(370, 12);
+            this.OriginalPictureBox.Name = "OriginalPictureBox";
+            this.OriginalPictureBox.Size = new System.Drawing.Size(400, 300);
+            this.OriginalPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.OriginalPictureBox.TabIndex = 0;
+            this.OriginalPictureBox.TabStop = false;
+            this.OriginalPictureBox.Click += new System.EventHandler(this.OriginalPictureBox_Click);
+            // 
+            // resultPictureBox
+            // 
+            this.resultPictureBox.Location = new System.Drawing.Point(370, 328);
+            this.resultPictureBox.Name = "resultPictureBox";
+            this.resultPictureBox.Size = new System.Drawing.Size(400, 300);
+            this.resultPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.resultPictureBox.TabIndex = 1;
+            this.resultPictureBox.TabStop = false;
+            this.resultPictureBox.Click += new System.EventHandler(this.resultPictureBox_Click);
+            // 
+            // FiltersComboBox
+            // 
+            this.FiltersComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.FiltersComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.FiltersComboBox.FormattingEnabled = true;
+            this.FiltersComboBox.Location = new System.Drawing.Point(12, 12);
+            this.FiltersComboBox.Name = "FiltersComboBox";
+            this.FiltersComboBox.Size = new System.Drawing.Size(340, 28);
+            this.FiltersComboBox.TabIndex = 2;
+            this.FiltersComboBox.SelectedIndexChanged += new System.EventHandler(this.FiltersComboBox_SelectedIndexChanged);
+            // 
+            // ApplyButton
+            // 
+            this.ApplyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.ApplyButton.Location = new System.Drawing.Point(98, 592);
+            this.ApplyButton.Name = "ApplyButton";
+            this.ApplyButton.Size = new System.Drawing.Size(174, 36);
+            this.ApplyButton.TabIndex = 3;
+            this.ApplyButton.Text = "Accept";
+            this.ApplyButton.UseVisualStyleBackColor = true;
+            this.ApplyButton.Visible = false;
+            this.ApplyButton.Click += new System.EventHandler(this.ApplyButton_Click);
             // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(782, 653);
+            this.Controls.Add(this.ApplyButton);
+            this.Controls.Add(this.FiltersComboBox);
+            this.Controls.Add(this.resultPictureBox);
+            this.Controls.Add(this.OriginalPictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "mainForm";
             this.Text = "PhotoEnxancer";
+            ((System.ComponentModel.ISupportInitialize)(this.OriginalPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resultPictureBox)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox OriginalPictureBox;
+        private System.Windows.Forms.PictureBox resultPictureBox;
+        private System.Windows.Forms.ComboBox FiltersComboBox;
+        private System.Windows.Forms.Button ApplyButton;
     }
 }
 
--- PhotoEnxancer/PhotoEnxancer/mainForm.cs
+++ PhotoEnxancer/PhotoEnxancer/mainForm.cs
@@ -12,9 +12,108 @@ namespace PhotoEnxancer
 {
     public partial class mainForm : Form
     {
+
+        Bitmap originalBmp;
+        Bitmap ResultBmp;
+
+        Panel parametersPanel;
+
         public mainForm()
         {
             InitializeComponent();
+
+            originalBmp = (Bitmap)Image.FromFile("SmallStark.jpg");
+            OriginalPictureBox.Image = originalBmp;
+
+            FiltersComboBox.Items.Add("Brightness");
+        }
+
+        private void resultPictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OriginalPictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FiltersComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ApplyButton.Visible = true;
+            
+            if (parametersPanel != null)
+                this.Controls.Remove(parametersPanel);
+
+            parametersPanel = new Panel();
+
+            parametersPanel.Left = FiltersComboBox.Left;
+            parametersPanel.Top = FiltersComboBox.Bottom + 10;
+            parametersPanel.Width = FiltersComboBox.Width;
+            parametersPanel.Height = ApplyButton.Top - FiltersComboBox.Bottom - 20;
+
+            //parametersPanel.BackColor= Color.Gray;
+
+            this.Controls.Add(parametersPanel);
+
+            var filter = FiltersComboBox.SelectedItem;
+
+            if(FiltersComboBox.SelectedItem.ToString() == "Brightness")
+            {
+                var label = new Label();
+                label.Left = 0;
+                label.Top = 0;
+                label.Width = parametersPanel.Width - 50;
+                label.Height = 28;
+                label.Text = "Ratio";
+                label.Font = new Font(label.Font.FontFamily, 10);
+                parametersPanel.Controls.Add(label);
+
+                var inputBox = new NumericUpDown();
+                inputBox.Left = label.Right;
+                inputBox.Top = label.Top;
+                inputBox.Width = parametersPanel.Width - label.Width;
+                inputBox.Height = label.Height;
+                inputBox.Font = new Font(inputBox.Font.FontFamily, 10);
+                inputBox.Minimum = 0;
+                inputBox.Maximum = 10;
+                inputBox.Increment = (decimal)0.05;
+                inputBox.DecimalPlaces = 2;
+                inputBox.Value = 1;
+                inputBox.Name = "Ratio";
+                parametersPanel.Controls.Add(inputBox);
+            }
+        }
+
+        private void ApplyButton_Click(object sender, EventArgs e)
+        {
+            var newBmp = new Bitmap(originalBmp.Width, originalBmp.Height);
+            if (FiltersComboBox.SelectedItem.ToString() == "Brightness")
+            {
+                var k = (double)((NumericUpDown)parametersPanel.Controls["Ratio"]).Value;
+                //MessageBox.Show(k.ToString());
+
+                for(var x = 0; x < originalBmp.Width; x++)
+                    for(var y=0;  y<originalBmp.Height; y++)
+                    {
+                        var pixelColor = originalBmp.GetPixel(x, y);
+                        var newR = (int)(pixelColor.R * k);
+                        if(newR > 255)
+                            newR = 255;
+
+                        var newG = (int)(pixelColor.G * k);
+                        if (newG > 255)
+                            newG = 255;
+
+                        var newB = (int)(pixelColor.B * k);
+                        if (newB > 255)
+                            newB = 255;
+
+                        newBmp.SetPixel(x, y, Color.FromArgb(newR, newG, newB));
+                    }
+            }
+            ResultBmp = newBmp;
+            resultPictureBox.Image = ResultBmp;
         }
     }
 }
